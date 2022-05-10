5540961853d5d0a73ea47ea33102346690c00712
Manfred-Adomako <105299096+Manfred-Adomako@users.noreply.github.com>
2022-05-10 17:37:23 +0200

    Create A1_var3.py

---
+++ K8/A1_var3.py
@@ -0,0 +1,17 @@
+#K8_A1 beispiel
+
+x= int(26)
+y= int(64)
+if x>y:
+    print("x is greater")
+elif x<y:
+    print("y is greater")
+
+#K8-A1
+
+def greater_number(number1, number2):
+    if number1 > number2:
+        print   (number1)
+    elif number1 < number2:
+        print   (number2)
+greater_number(22,24)      #Neue werte eintragen und testen
